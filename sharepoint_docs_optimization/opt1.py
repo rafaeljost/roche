61bdbc217e46f479fb1f8ea984abb3ca0ac13a0a
Rafael Jost <rafamjf@gmail.com>
2018-03-27 20:25:00 +0000

    Testing directories

--- sharepoint_docs_optimization/opt1.py
+++ sharepoint_docs_optimization/opt1.py
@@ -1,14 +1,29 @@
-print "Hello World!"
+#Program Title
+print ("Opt1!")
 
+#necessary imports
 import datefinder
+    
+
+#Module 1: filter date out of a string
 
 input_string = "monkey 2010-07-10 love banana"
-# a generator will be returned by the datefinder module. I'm typecasting it to a list. Please read the note of caution provided at the bottom.
-matches = list(datefinder.find_dates(input_string))
 
-if len(matches) > 0:
-    # date returned will be a datetime.datetime object. here we are only using the first match.
-    date = matches[0]
-    print date
-else:
-    print 'No dates found'
+def filter_date(string): 
+   
+    # a generator will be returned by the datefinder module. I'm typecasting it to a list. Please read the note of caution provided at the bottom.
+    matches = list(datefinder.find_dates(string))
+
+    if len(matches) > 0:
+        # date returned will be a datetime.datetime object. here we are only using the first match.
+        date = matches[0]
+        print (date)
+    else:
+        print ('No dates found')
+        
+    return
+
+filter_date(input_string)
+
+
+
